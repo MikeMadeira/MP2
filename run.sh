c4e264aa6e5635dcec6c025b028526aaab91c145
Mike Madeira <michael-madeira@hotmail.com>
2020-11-09 01:20:17 +0000

    hours transducer first try

--- run.sh
+++ run.sh
@@ -18,3 +18,6 @@ done
 
 echo "Testing the transducer 'converter' with the input 'tests/numero.txt'"
 fstcompose compiled/numero.fst compiled/converter.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+
+echo "Testing the transducer 'hours' with the input 'tests/sleepA94129.txt'"
+fstcompose compiled/sleepA94129.fst compiled/hours.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt

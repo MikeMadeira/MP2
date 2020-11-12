e91db70559c8c10b147479cabbfd963e4f1ea544
Mike Madeira <michael-madeira@hotmail.com>
2020-11-12 19:09:04 +0000

    Simple Transducers

--- run.sh
+++ run.sh
@@ -19,5 +19,11 @@ done
 echo "Testing the transducer 'converter' with the input 'tests/numero.txt'"
 fstcompose compiled/numero.fst compiled/converter.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
 
-echo "Testing the transducer 'hours' with the input 'tests/sleepA94129.txt'"
-fstcompose compiled/sleepA94129.fst compiled/hours.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+echo "Testing the transducer 'hours' with the input 'tests/sleepB94129.txt'"
+fstcompose compiled/sleepB94129.fst compiled/hours.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+
+echo "Testing the transducer 'minutos' with the input 'tests/wakeupB94129.txt'"
+fstcompose compiled/wakeupB94129.fst compiled/minutos.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+
+echo "Testing the transducer 'meias' with the input 'tests/wakeupA94129.txt'"
+fstcompose compiled/wakeupA94129.fst compiled/meias.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt

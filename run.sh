144be2b635b322faea00d3b08e6c792fb8d30f80
Mike Madeira <michael-madeira@hotmail.com>
2020-11-14 23:06:56 +0000

    text2num done less oclock hours

--- run.sh
+++ run.sh
@@ -8,7 +8,11 @@ for i in sources/*.txt tests/*.txt; do
 done
 
 
-# TODO
+echo "Concat the transducer 'hours' with the transducer 'aux_e'"
+fstconcat compiled/hours.fst compiled/aux_e.fst compiled/hours_e.fst
+
+echo "Concat the transducer 'hours_e' with the transducer 'minutos'"
+fstconcat compiled/hours_e.fst compiled/minutos.fst compiled/text2num.fst
 
 
 for i in compiled/*.fst; do
@@ -27,3 +31,9 @@ fstcompose compiled/wakeupB94129.fst compiled/minutos.fst | fstshortestpath | fs
 
 echo "Testing the transducer 'meias' with the input 'tests/wakeupA94129.txt'"
 fstcompose compiled/wakeupA94129.fst compiled/meias.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+
+echo "Testing the transducer 'trans_teste' with the input 'tests/teste1.txt'"
+fstcompose compiled/teste1.fst compiled/trans_teste.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
+
+echo "Testing the transducer 'text2num' with the input 'tests/sleepB94129.txt'"
+fstcompose compiled/sleepB94129.fst compiled/text2num.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
